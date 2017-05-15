aceed6cff53b452c67d51cafdea6e5791bf2f35b
DOITSKY\jason <jasonwang@doitsky.com>
2017-05-15 11:29:12 +0800

    Init Project

---
+++ app/src/main/java/idv/jasonwang/circleimageview/sample/MainActivity.java
@@ -0,0 +1,14 @@
+package idv.jasonwang.circleimageview.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
